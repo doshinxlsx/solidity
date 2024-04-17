88fa0a01f47369af471c3e8d8ffeb0daac160ef5
doshinxlsx <o.dominik2107@gmail.com>
2024-04-17 12:46:46 +0200

    feat(twitter): create and emit events to functions

--- contracts/Twitter.sol
+++ contracts/Twitter.sol
@@ -16,6 +16,10 @@ contract Twitter {
     mapping(address => Tweet[]) public tweets;
     address public owner;
 
+    event TweetCreated(uint256 id, address author, string content, uint256 timestamp);
+    event TweetLiked(address liker, address tweetAuthor, uint256 tweetId, uint256 newLikeCount);
+    event TweetUnliked(address unliker, address tweetAuthor, uint256 tweetId, uint256 newLikeCount);
+
     constructor() {
         owner = msg.sender;
     }
@@ -41,12 +45,14 @@ contract Twitter {
         });
 
         tweets[msg.sender].push(newTweet);
+        emit TweetCreated(newTweet.id, newTweet.author, newTweet.content, newTweet.timestamp);
     }
 
     function likeTweet(address author, uint256 id) external {
         require(tweets[author][id].id == id, "Tweet doesn't exist.");
 
         tweets[author][id].likes++;
+        emit TweetLiked(msg.sender, author, id, tweets[author][id].likes);
     }
 
     function unlikeTweet(address author, uint256 id) external {
@@ -54,6 +60,7 @@ contract Twitter {
         require(tweets[author][id].likes > 0, "Tweet has no likes");
 
         tweets[author][id].likes--;
+        emit TweetUnliked(msg.sender, author, id, tweets[author][id].likes);
     }
 
     function getTweet(uint _i) public view returns(Tweet memory) {
